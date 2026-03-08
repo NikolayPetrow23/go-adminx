0d0f93b05ee4de10755ed17600971d7a5ff92481
idiscount <idiscount@iMac-Nikolaj.local>
2026-03-08 19:03:36 +0700

    update ui.

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	
+	"go-adminx/internal/router"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", static))
+	mux.HandleFunc("/", router.IndexRouter)
+	mux.HandleFunc("/orderuid", router.OrderRouter)
+	log.Println("Запуск сервера на http://127.0.0.1:8000")
+	err := http.ListenAndServe(":8000", mux)
+	log.Fatal(err)
+}
